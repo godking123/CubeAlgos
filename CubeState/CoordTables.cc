a2dc069447bcb1c2fd1c9db763b1cd3719f4b07f
godking123 <godking123@users.noreply.github.com>
2026-08-30 22:30:31 -0400

    Created prune tables through BFS from solved state

--- CubeState/CoordTables.cc
+++ CubeState/CoordTables.cc
@@ -7,6 +7,9 @@ int flipMove[2048][18];
 int twistMove[2187][18];
 int sliceMove[495][18];
 
+int pruneFlipSlice[2048][495];
+int pruneTwistSlice[2187][495];
+
 void buildCoordTables() {
     // flip move table
     for (int flip = 0; flip < 2048; flip++) {
@@ -36,5 +39,71 @@ void buildCoordTables() {
             CubeState next = s.apply(static_cast<Move>(m));
             sliceMove[slice][m] = Coords::encodeSlice(next);
         }
+    }
+}
+
+void buildPruningTables() {
+    // flip prune table
+    for (int f = 0; f < 2048; f++) {
+            for (int s = 0; s < 495; s++) {
+            pruneFlipSlice[f][s] = -1;
+        }
+    }
+    
+    pruneFlipSlice[0][0] = 0;
+    int count = 1;
+    int depth = 0;
+
+    while (count < 2048 * 495) {
+        // scan for current depth
+        for (int f = 0; f < 2048; f++) {
+            for (int s = 0; s < 495; s++) {
+                if (pruneFlipSlice[f][s] == depth) {
+                    for (int m = 0; m < 18; m++) {
+                        int newF = flipMove[f][m];
+                        int newS = sliceMove[s][m];
+
+                        if (pruneFlipSlice[newF][newS] == -1) {
+                            pruneFlipSlice[newF][newS] = depth + 1;
+                            count++;
+                        }
+
+                    }                
+                }
+            }
+        }
+        depth++;
+    }
+
+    // twist prune table
+    for (int f = 0; f < 2187; f++) {
+        for (int s = 0; s < 495; s++) {
+            pruneTwistSlice[f][s] = -1;
+        }
+    }
+    
+    pruneTwistSlice[0][0] = 0;
+    count = 1;
+    depth = 0;
+
+    while (count < 2187 * 495) {
+        // scan for current depth
+        for (int f = 0; f < 2187; f++) {
+            for (int s = 0; s < 495; s++) {
+                if (pruneTwistSlice[f][s] == depth) {
+                    for (int m = 0; m < 18; m++) {
+                        int newF = twistMove[f][m];
+                        int newS = sliceMove[s][m];
+
+                        if (pruneTwistSlice[newF][newS] == -1) {
+                            pruneTwistSlice[newF][newS] = depth + 1;
+                            count++;
+                        }
+
+                    }                
+                }
+            }
+        }
+        depth++;
     }
 }
--- CubeState/CoordTables.h
+++ CubeState/CoordTables.h
@@ -5,6 +5,10 @@ extern int flipMove[2048][18];
 extern int twistMove[2187][18];
 extern int sliceMove[495][18];
 
+extern int pruneFlipSlice[2048][495];
+extern int pruneTwistSlice[2187][495];
+
+void buildPruningTables();
 void buildCoordTables();  // call once at startup in main
 
 #endif
