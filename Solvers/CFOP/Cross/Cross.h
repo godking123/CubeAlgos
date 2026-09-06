679f204adca782cd9a0cca784d224cfccc726d04
godking123 <godking123@users.noreply.github.com>
2026-09-05 20:59:04 -0500

    test: implemented cross tests with random scrambles

---
+++ Solvers/CFOP/Cross/Cross.h
@@ -0,0 +1,12 @@
+#ifndef CROSS_H
+#define CROSS_H
+
+#include <vector>
+#include "../../../CubeState/CubeState.h"
+
+namespace Cross {
+    bool isSolved(const CubeState& s);
+    std::vector<Move> solveCross(const CubeState& s);
+}
+
+#endif // CROSS_H_
